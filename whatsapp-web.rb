453c863e1174cc3ecddc1a7c63ac5ecb703bef4b
Takashi Yoshida <takashi@takashiyoshida.org>
2014-07-23 00:32:38 +0800

    Fix an issue where names of two or more people get assigned to `name`

--- whatsapp-web.rb
+++ whatsapp-web.rb
@@ -9,10 +9,8 @@ if __FILE__ == $0
   # regex for detecting the beginning of a message
   # each message should begin with a date time stamp
   msg_begin = /(\d{1,2}\/\d{1,2}\/\d{2} \d{1,2}:\d{2}:\d{2} [ap]m): (.+)/
-  # detect person and the following action
-  person = /(.+): (.+)/
   omitted = /<media omitted>$/
-  attached = /<attached>$/
+  attached = /(.+) <attached>$/
 
   data = File.open(DATAFILE, 'r')
   data.each do | line |
@@ -20,27 +18,29 @@ if __FILE__ == $0
       # create a DateTime object from a string (timezone is set to SGT)
       #puts "stamp: #{msg[1]}"
       stamp = DateTime.strptime("#{msg[1]} SGT", "%d/%m/%y %l:%M:%S %P %Z")
-      
-      if p = person.match(msg[2].chomp)
-        name = p[1]
-        action = "says"
+      line = msg[2].chomp
 
-        if o = omitted.match(p[2].chomp)
-          puts "<BADABING>"
+      if line.index(':').nil?
+        puts "status: #{stamp}: #{line}"
+      else
+        index = line.index(':')
+        name = line.slice(0..index - 1)
+        line = line.slice(index + 1..-1).strip
+
+        action = "says"
+        if o = omitted.match(line)
           action = "<media omitted>"
-        elsif a = attached.match(p[2].chomp)
-          puts "<BADABOOM>"
+        elsif a = attached.match(line)
           action = "uploads"
+          line = a[1] 
         end
 
         if action == "<media omitted>"
           puts "action: #{stamp}: #{name}: #{action}"
         else
           puts "action: #{stamp}: #{name} #{action}:"
-          puts "#{p[2]}"
+          puts "#{line}"
         end
-      else
-        puts "status: #{stamp}: #{msg[2]}"
       end
     else
       puts "... #{line.chomp}"
