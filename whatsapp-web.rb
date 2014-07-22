16c21ce3b49556a3d94c7a3af252f8480c829d66
Takashi Yoshida <takashi@takashiyoshida.org>
2014-07-22 23:01:59 +0800

    A small number of clean up before generating HTML files

--- whatsapp-web.rb
+++ whatsapp-web.rb
@@ -17,22 +17,33 @@ if __FILE__ == $0
   data = File.open(DATAFILE, 'r')
   data.each do | line |
     if msg = msg_begin.match(line.chomp)
-      # create a DateTime object from a string
+      # create a DateTime object from a string (timezone is set to SGT)
       #puts "stamp: #{msg[1]}"
       stamp = DateTime.strptime("#{msg[1]} SGT", "%d/%m/%y %l:%M:%S %P %Z")
       
       if p = person.match(msg[2].chomp)
         name = p[1]
+        action = "says"
 
         if o = omitted.match(p[2].chomp)
           puts "<BADABING>"
+          action = "<media omitted>"
         elsif a = attached.match(p[2].chomp)
           puts "<BADABOOM>"
+          action = "uploads"
         end
-        puts "action: #{stamp}: #{name}: #{p[2]}"
+
+        if action == "<media omitted>"
+          puts "action: #{stamp}: #{name}: #{action}"
+        else
+          puts "action: #{stamp}: #{name} #{action}:"
+          puts "#{p[2]}"
+        end
       else
         puts "status: #{stamp}: #{msg[2]}"
       end
+    else
+      puts "... #{line.chomp}"
     end
   end
 end
